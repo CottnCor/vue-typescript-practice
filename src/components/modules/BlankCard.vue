c25f33a2e2f0dbedcbeabf5b8977438633df1e8f
CottnCor <gaotaomo@live.com>
2019-05-30 07:51:36 +0800

    :lipstick: :construction:  fix

--- src/components/modules/BlankCard.vue
+++ src/components/modules/BlankCard.vue
@@ -1,33 +1,17 @@
 <template>
-  <a-card hoverable style="width: 300px">
+  <a-card loading="true" hoverable style="width: 300px">
+    <template class="ant-card-actions" slot="title">
+      <a-icon type="setting" />
+      <span>我的肥宅生活</span>
+    </template>
     <img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" slot="cover" />
-    <a-table :columns="columns" :dataSource="data" bordered slot="cover">
-      <template slot="name" slot-scope="text">
-        <a href="javascript:;">{{text}}</a>
-      </template>
-      <template slot="action" slot-scope="text, record">
-        <span>
-          <a href="javascript:;">Action 一 {{record.name}}</a>
-          <a-divider type="vertical" />
-          <a href="javascript:;">Delete</a>
-          <a-divider type="vertical" />
-          <a href="javascript:;" class="ant-dropdown-link">
-            More actions
-            <a-icon type="down" />
-          </a>
-        </span>
-      </template>
-    </a-table>
-    <a-card-meta>
-
-    </a-card-meta>
+    <a-table :columns="columns" :dataSource="data" :pagination="false" slot="cover" />
     <a-card-meta title="Card title" description="This is the description">
       <a-avatar slot="avatar" src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
     </a-card-meta>
-    <template class="ant-card-actions" slot="actions">
+    <template class="ant-card-actions" slot="extra">
       <a-icon type="setting" />
       <a-icon type="edit" />
-      <a-icon type="ellipsis" />
     </template>
   </a-card>
 </template>
@@ -36,7 +20,47 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({})
-class BlankCard extends Vue {}
+class BlankCard extends Vue {
+    private columns = [
+        {
+            dataIndex: 'name',
+            key: 'name',
+            slots: { title: 'customTitle' },
+            scopedSlots: { customRender: 'name' }
+        },
+        {
+            title: 'Age',
+            dataIndex: 'age',
+            key: 'age'
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address'
+        }
+    ];
+
+    private data = [
+        {
+            key: '1',
+            name: 'John Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park'
+        },
+        {
+            key: '2',
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 1 Lake Park'
+        },
+        {
+            key: '3',
+            name: 'Joe Black',
+            age: 32,
+            address: 'Sidney No. 1 Lake Park'
+        }
+    ];
+}
 export default BlankCard;
 </script>
 
